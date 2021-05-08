7212113888f1b499929ba5a8de502adfaf7e6b44
divij <divij@uw.edu>
2021-05-07 18:39:48 -0700

    Initial Commit

---
+++ iKid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Divij Satija on 5/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
